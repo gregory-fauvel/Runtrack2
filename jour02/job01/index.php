bd19cb3f62840d3efaf7f5af4d5a959e326b9398
fauvel gregory <gregory.fauvel@laplateforme.io>
2019-10-29 11:14:45 +0100

    job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+	$debut=0;
+
+
+	while ($debut!=1338) {
+		echo $debut=$debut+1;
+		echo "<br>";
+
+		if ($debut==42) 
+		{
+			echo "<b><u>$debut</b></u>";
+			echo "<br>";
+		}
+		
+	}
+
+?>
