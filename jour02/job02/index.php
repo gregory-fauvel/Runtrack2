7a3252e6214a4ec3800b46c2e41f059ee5aa65ae
fauvel gregory <gregory.fauvel@laplateforme.io>
2019-10-29 12:59:08 +0100

    job2

---
+++ jour02/job02/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$debut= 0;
+
+
+ while ($debut != 1337)
+ {
+    if($debut==26||$debut==37||$debut==88||$debut==1111||$debut==3233)
+    {
+        $debut =$debut+1;
+    }
+    else{
+        echo $debut;
+        echo "<br>";
+        $debut =$debut+1;
+
+    }
+}
+    
+    ?>
+      
